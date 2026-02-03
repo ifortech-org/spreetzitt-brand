d85f02fd9d8fdfdeaa54f0acdf9246e5d28ad598
erasmosalsanoifortech <e.salsano@ifortech.com>
2026-02-03 19:13:18 +0100

    Modificato footer. se da errore, la modifica è più lunga

--- shared/components/footer.tsx
+++ shared/components/footer.tsx
@@ -46,6 +46,9 @@ export default function Footer() {
           ))}
         </div>
         <div className="mt-8 flex flex-col lg:flex-row gap-6 justify-center text-center lg:mt-5 text-xs border-t pt-8">
+          <p className="text-xs font-thin">Spreetzit è un marchio iFortech srl</p>
+          <p className="text-xs font-thin">CAP. SOC. € 40.000,00 I.V. - P.IVA E CF: 07927140967 - REA: MI-1991600</p>
+          <p className="text-xs font-thin">SEDE LEGALE: VIA PORDENONE 35 COLOGNO MONZESE - 20093 (MI)</p>
           <p className="text-foreground/60">
             &copy; {getCurrentYear()}&nbsp;iFortech. All rights reserved.
           </p>
